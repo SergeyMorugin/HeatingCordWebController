43bda304cc5e503429a598158c7242a712c6ae60
Sergey Morugin <cmorugin@gmail.com>
2020-02-01 19:18:28 +0300

    added for now basic http authentication for dangeros internet

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'whenever', :require => false
 
 gem 'serialport'
+ 
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+       username == ENV['HTTP_BASIC_AUTHENTICATION_USERNAME'] && 
+         password == ENV['HTTP_BASIC_AUTHENTICATION_PASSWORD']
+    end
+  end
+
 end
