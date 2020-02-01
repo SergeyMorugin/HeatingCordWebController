b6f4ee8c35cc5df8cb9368012b9eed5c662c39d0
Sergey Morugin <cmorugin@gmail.com>
2020-02-01 23:19:08 +0300

    fixes for ArduinoWeatherProvider

--- app/lib/arduino_weather_provider.rb
+++ app/lib/arduino_weather_provider.rb
@@ -5,9 +5,16 @@ class ArduinoWeatherProvider
     @address = address
   end
 
-  def get_wheather
+  def get_weather
     response = @io_dev.get_data
     return response if response[:status] != 'OK'
+
+    unless (response[:body].include?('[') and
+        response[:body].include?('|') and
+        response[:body].include?(']'))
+      return {status: 'Error', error_message: 'Unknown format', body: response[:body]} 
+    end 
+
     begin
       response_data = response[:body].delete('[').split(']').first.split('|')
       temperature = response_data[0].to_f
@@ -16,20 +23,7 @@ class ArduinoWeatherProvider
       rainfall = response_data[3].to_i
       {status: 'OK', data: {temperature: temperature, pressure: pressure, humidity: humidity, rainfall: rainfall}}
     rescue => e
-      return {status: 'Error', error_message: 'Unknown format', body: response['body']}
+      return {status: 'Error', error_message: 'Unknown format', body: response[:body]}
     end  
   end
-
-private
-  
-  
-
 end
-
-=begin
-  d = '/dev/ttyUSB1'
-  sp = SerialportGateway.new(d)
-  a = ArduinoWeatherProvider.new(sp)
-  puts a.get_measure
-	
-=end
--- lib/tasks/arduino.rake
+++ lib/tasks/arduino.rake
@@ -2,7 +2,7 @@ namespace :arduino do
   task :get_weather => :environment do  	
     gateway = SerialportGateway.new(ENV['ANDUINO_SERIAL_PORT'])
     arduino = ArduinoWeatherProvider.new(gateway)
-    response = arduino.get_wheather   
+    response = arduino.get_weather   
     next if response[:status] != 'OK'     
     WeatherMeasure.create!(response[:data])     
   end
