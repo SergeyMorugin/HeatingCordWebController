9cd3b23540d7acb42cde1743f84984a28bf4f3f7
Sergey Morugin <cmorugin@gmail.com>
2020-02-18 12:21:53 +0300

    refactoring for HEATIN_CORD_AUTOMATIC_MODE constant

--- app/models/heating_cord.rb
+++ app/models/heating_cord.rb
@@ -1,7 +1,7 @@
 class HeatingCord < ApplicationRecord
   HEATIN_CORD_OFF_MODE = 0
   HEATIN_CORD_MANUAL_ON_MODE = 1
-  HEATIN_CORD_AUTOMATIC_MODE = 10 #switching every hour to on or off
+  HEATIN_CORD_AUTOMATIC_1H_SWITCH_MODE = 10 #switching every hour to on or off
 
 
   def update_mode(new_mode)  	
@@ -14,7 +14,7 @@ class HeatingCord < ApplicationRecord
   	  	result = meter.disable_consumer
   	  when HEATIN_CORD_MANUAL_ON_MODE then
         result = meter.enable_consumer
-  	  when HEATIN_CORD_AUTOMATIC_MODE then
+  	  when HEATIN_CORD_AUTOMATIC_1H_SWITCH_MODE then
   	    #puts meter.enable_consumer 
   	end
     update!(mode: new_mode)
@@ -25,7 +25,7 @@ class HeatingCord < ApplicationRecord
   	case mode 
   	  when HEATIN_CORD_OFF_MODE then 'HEATIN_CORD_OFF_MODE'
   	  when HEATIN_CORD_MANUAL_ON_MODE then 'HEATIN_CORD_MANUAL_ON_MODE'
-  	  when HEATIN_CORD_AUTOMATIC_MODE then 'HEATIN_CORD_AUTOMATIC_MODE'
+  	  when HEATIN_CORD_AUTOMATIC_1H_SWITCH_MODE then 'HEATIN_CORD_AUTOMATIC_1H_SWITCH_MODE'
   	  else 'Undefinded'
   	end	
 
