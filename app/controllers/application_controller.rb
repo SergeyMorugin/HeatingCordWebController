0534eab8bc65b8a0acf401f449adbcaa292fac93
Sergey Morugin <cmorugin@gmail.com>
2020-04-23 12:30:59 +0300

    add http auth just for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate
+  before_action :authenticate, if: :production_mode?
 
   protected
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-       username == ENV['HTTP_BASIC_AUTHENTICATION_USERNAME'] && 
-         password == ENV['HTTP_BASIC_AUTHENTICATION_PASSWORD']
+      username == ENV['HTTP_BASIC_AUTHENTICATION_USERNAME'] && 
+      password == ENV['HTTP_BASIC_AUTHENTICATION_PASSWORD']
     end
   end
+
+  def production_mode?
+  	Rails.env.production?
+   end 	
 
 end
