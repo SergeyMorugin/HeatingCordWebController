e59ccf8c7e1538cf381ed78443302e3ace80ffc8
Sergei Morugin <cmorugin@gmail.com>
2021-03-04 11:56:32 +0300

    add switcher

--- app/lib/home_assistant.rb
+++ app/lib/home_assistant.rb
@@ -63,7 +63,8 @@ class HomeAssistant
   def register_devise(mqtt_client, base, devise)
     request = devise_struct(devise)
 
-    mqtt_client.publish "#{base['id']}/bridge/devices", request.to_json, true
+    #mqtt_client.publish "#{base['id']}/bridge/devices", request.to_json, true
+    p "#{base['id']}/bridge/devices", request.to_json
   end
 
   def devise_struct(devise)
@@ -74,8 +75,15 @@ class HomeAssistant
                 "description": devise['model'],
                 "exposes":
                     devise['parameters'].map do |param|
-                        param['access'] = 1
-                        param['property'] = param['name']
+                        case param['parameter_type']
+                        when 'senser'
+                          param['access'] = 1
+                          param['property'] = param['name']
+                        when 'switcher'
+                        else
+                          raise
+                        end
+                        param['parameter_type'] = nil
                         param
                     end,
                 "model": devise['model'],
@@ -95,7 +103,7 @@ class HomeAssistant
             #"ieee_address":"0x00158d000205d553",
             "interview_completed":true,
             "interviewing":false,
-            "model_id":"FNB56-COS06FB1.7",
+            #"model_id":"FNB56-COS06FB1.7",
             #"network_address":9914,
             #"power_source":"Battery",
             "software_build_id":"\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000",
@@ -113,14 +121,68 @@ class HomeAssistant
                                devise['vendor'],
                                devise['model'],
                                parameter['icon'])
+    case parameter['parameter_type']
+    when 'senser'
+      request = senser_parameter_struct(base, devise, parameter)
+    when 'switcher'
+      request = switcher_parameter_struct(base, devise, parameter)
+    else
+      raise
+    end
 
-    mqtt_client.publish("homeassistant/sensor/#{devise['id']}/#{parameter['name']}/config", request.to_json, true)
+    #mqtt_client.publish("homeassistant/#{parameter['parameter_type']}/#{devise['id']}/#{parameter['name']}/config", request.to_json, true)
     #mqtt_client.publish "test/test", request.to_json
+    p "homeassistant/#{parameter['parameter_type']}/#{devise['id']}/#{parameter['name']}/config", request
   end
 
 
   # ha = HomeAssistant.new
   # ha.register_parameter(DEVISES_CONF['bases'][0], DEVISES_CONF['bases'][0]['devises'][0], DEVISES_CONF['bases'][0]['devises'][0]['parameters'][0])
+
+  def senser_parameter_struct(base, devise, param)
+    {
+        "availability":[{"topic":"#{base['id']}/bridge/state"}],
+        "device":{
+            "identifiers":["#{base['id']}_#{devise['id']}"],
+            "manufacturer": devise['vendor'],
+            "model": devise['model'],
+            "name":"#{devise['id']}",
+            #"sw_version":"#{base['id']} 0.0.1"
+        },
+        "device_class": param['name'],
+        #"icon": icon,
+        "json_attributes_topic":"#{base['id']}/#{devise['id']}",
+        "name":"#{devise['id']} #{param['name']}",
+        "state_topic":"#{base['id']}/#{devise['id']}",
+        "unique_id":"#{devise['id']}_#{param['name']}_#{base['id']}",
+        "value_template":"{{ value_json.#{param['name']} }}",
+        "unit_of_measurement": param['unit'],
+    }
+  end
+
+  def switcher_parameter_struct(base, devise, param)
+    {
+        "command_topic":"#{base['id']}/#{devise['id']}/#{param['name']}/set",
+        "payload_off":"OFF",
+        "payload_on":"ON",
+
+        "availability":[{"topic":"#{base['id']}/bridge/state"}],
+        "device":{
+            "identifiers":["#{base['id']}_#{devise['id']}"],
+            "manufacturer": devise['vendor'],
+            "model": devise['model'],
+            "name":"#{devise['id']}",
+            #"sw_version":"#{base['id']} 0.0.1"
+        },
+        "device_class": param['name'],
+        #"icon": icon,
+        "json_attributes_topic":"#{base['id']}/#{devise['id']}",
+        "name":"#{devise['id']} #{param['name']}",
+        "state_topic":"#{base['id']}/#{devise['id']}",
+        "unique_id":"#{devise['id']}_#{param['name']}_#{base['id']}",
+        "value_template":"{{ value_json.#{param['name']} }}",
+    }
+  end
 
   def parameter_struct(base_id, dev_id, param_id, unit, manufacturer="Ostagram", model="", icon="mdi:signal")
     {
